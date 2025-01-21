11c01d00980ac08cdb5352c05360017cd8632e23
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-21 17:08:41 +0100

    Refactor tab layout and remove unused components
    - Updated tab layout styles, changing active tint color to #F57230 and background color to #FFEFD7.
    - Removed TabBarBackground component for iOS and its associated functionality, simplifying the tab bar implementation.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,9 @@
 import { Redirect, Tabs } from 'expo-router';
 import React from 'react';
-import { Platform, useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { useAuth } from '@clerk/clerk-expo';
 
 export default function TabLayout() {
@@ -18,17 +17,12 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'black',
+        tabBarActiveTintColor: '#F57230',
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        tabBarStyle: {
+          backgroundColor: '#FFEFD7',
+        },
       }}
     >
       <Tabs.Screen
