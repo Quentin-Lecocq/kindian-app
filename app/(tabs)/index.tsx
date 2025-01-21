587f3408989a5cff5ab9d0b3895ab90e51b22d93
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-21 16:56:38 +0100

    Enhance Account and Home screens with improved UI and sign-out functionality
    - Added a PrimaryButton for signing out on the Account screen.
    - Updated Home screen layout with a new header design, incorporating Menu and Bell icons.
    - Improved styling and structure for better user experience and visual consistency.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,36 @@
-import { SignedIn, SignedOut, useAuth, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
-import { Button, SafeAreaView, Text } from 'react-native';
+import { SignedIn, useAuth, useUser } from '@clerk/clerk-expo';
+import { Bell, Menu } from 'iconoir-react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 export default function Home() {
   const { user } = useUser();
   const { signOut } = useAuth();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-secondary">
       <SignedIn>
-        <Text className="text-4xl my-8 font-bold text-center font-bodoni-moda-bold text-black">
-          KINDIAN
-        </Text>
-        <Text className="text-sm font-roboto-mono">
-          Hello {user?.emailAddresses[0].emailAddress}
-        </Text>
-        <Text className="text-sm font-roboto-mono">
-          You can now start using the app
-        </Text>
-        <Text className="text-sm mt-8 font-dm-mono-medium">
-          Lorem ipsum dolor sit amet consectetur forever adipisicing elit.
-          Quisquam, quos.
-        </Text>
-        <Button title="Sign out" onPress={() => signOut()} />
+        <View className="border flex-row justify-between items-center h-20">
+          <View className="border-r h-full items-center justify-center flex-row w-20">
+            <Menu width={22} height={22} strokeWidth={2} />
+          </View>
+          <View className="mt-3">
+            <Text className="text-3xl font-bold font-bodoni-moda-bold text-black">
+              KINDIAN
+            </Text>
+          </View>
+          <View className="border-l h-full items-center justify-center flex-row w-20">
+            <Bell width={22} height={22} strokeWidth={2} />
+          </View>
+        </View>
+        <View className="p-4">
+          <Text className="text-sm font-roboto-mono">
+            Hello {user?.emailAddresses[0].emailAddress}
+          </Text>
+          <Text className="text-sm font-roboto-mono">
+            You can now start using the app
+          </Text>
+        </View>
       </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
     </SafeAreaView>
   );
 }
